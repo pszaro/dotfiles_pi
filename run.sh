d4e92227b492438083a4c5232c7295fa27f98a29
pszaro <github@szaroconsulting.com>
2017-03-18 12:07:00 -0400

    Updated path

--- run.sh
+++ run.sh
@@ -12,8 +12,8 @@ set -o errexit
 set -o pipefail
 
 # LIBRARY
-source ~/git-repos/dotfile_pi/utilities.sh
-source ~/git-repos/dotfile_pi/options.sh
+source ~/git-repos/dotfiles_pi/utilities.sh
+source ~/git-repos/dotfiles_pi/options.sh
 
 # EXECUTION
 if [[ -d "$HOME" ]]; then
